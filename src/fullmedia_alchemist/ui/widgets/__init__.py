79a4989b31a731d9e37ceaa5947c082865f5a4f9
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-06 19:58:55 -0700

    Export tool-aware button widget

--- src/fullmedia_alchemist/ui/widgets/__init__.py
+++ src/fullmedia_alchemist/ui/widgets/__init__.py
@@ -3,5 +3,6 @@
 from .collapsible_section import CollapsibleSection
 from .drop_zone import DropZone
 from .footer_status_bar import FooterStatusBar
+from .tool_button import ToolButton
 
-__all__ = ["CollapsibleSection", "DropZone", "FooterStatusBar"]
+__all__ = ["CollapsibleSection", "DropZone", "FooterStatusBar", "ToolButton"]
