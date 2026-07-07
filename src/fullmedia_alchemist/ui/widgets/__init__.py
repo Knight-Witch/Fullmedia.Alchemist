4f797822c72644afceea5b409c8165f4236f1d36
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-06 19:56:56 -0700

    Add UI widgets package initializer

---
+++ src/fullmedia_alchemist/ui/widgets/__init__.py
@@ -0,0 +1,7 @@
+"""Reusable UI widgets."""
+
+from .collapsible_section import CollapsibleSection
+from .drop_zone import DropZone
+from .footer_status_bar import FooterStatusBar
+
+__all__ = ["CollapsibleSection", "DropZone", "FooterStatusBar"]
