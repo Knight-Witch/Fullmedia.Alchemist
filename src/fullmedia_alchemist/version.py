e7bcc313686ae8b8649689c2c66d8a08e963cf9b
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-06 19:09:50 -0700

    Add initial app version constants

---
+++ src/fullmedia_alchemist/version.py
@@ -0,0 +1,6 @@
+"""Application version metadata for Fullmedia Alchemist."""
+
+APP_NAME = "Fullmedia Alchemist"
+APP_VERSION = "Dev_v000.001.000_07-06-26"
+BUILD_DATE = "07-06-26"
+EXECUTABLE_NAME = "Fullmedia_Alchemist.exe"
