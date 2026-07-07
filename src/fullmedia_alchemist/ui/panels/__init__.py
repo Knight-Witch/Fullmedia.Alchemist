a95b861f5af4f3efcd2c0dfc9e6658a7a79f6c94
Knight Witch <amanda.m.ivans@gmail.com>
2026-07-06 19:57:23 -0700

    Add UI panels package initializer

---
+++ src/fullmedia_alchemist/ui/panels/__init__.py
@@ -0,0 +1,8 @@
+"""Main UI panel widgets."""
+
+from .inspector_panel import InspectorPanel
+from .profile_panel import ProfilePanel
+from .progress_panel import ProgressPanel
+from .queue_panel import QueuePanel
+
+__all__ = ["InspectorPanel", "ProfilePanel", "ProgressPanel", "QueuePanel"]
